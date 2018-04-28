b427ccdde37405f06f09cb51f8f61ae7c490894a
IlyaShuv <ilya.shuv24@gmail.com>
2018-04-28 14:21:59 +0300

    Доработано подключение к базе данных и занесение в неё информации о зарегистрировавшемся человеке.

--- app/login.php
+++ app/login.php
@@ -1,4 +1,9 @@
-<?php session_start();         
+<?php 
+	header('Content-Type: text/html; charset=utf-8');
+	mb_internal_encoding("UTF-8");
+	mysql_set_charset('utf8');
+	
+	session_start();         
   $s = file_get_contents('http://ulogin.ru/token.php?token=' . $_POST['token'] . '&host=' . $_SERVER['HTTP_HOST']);
   $user = json_decode($s, true);
   //$user['network'] - соц. сеть, через которую авторизовался пользователь
@@ -6,8 +11,21 @@
   //$user['first_name'] - имя пользователя
   //$user['last_name'] - фамилия пользователя
 
+  $network = $user['network'];
+  $identity = $user['identity'];
+  $first_name = $user['first_name'];
+  $last_name = $user['last_name'];
+
   if (isset($user))
   {
+  	#Соединение с БД
+  	$db = mysql_connect("localhost", "i40232_ksoloband", "2018ksoloband");
+  	mysql_select_db("i40232_ksoloband", $db);
+
+  	mysql_query("INSERT INTO people (first_name, last_name, network, identity) VALUES ('$first_name','$last_name','$network','$identity')");
+
+
+
   	$_SESSION['user'] = $user;
   	header("Location: reg_page.php");
   	exit;
